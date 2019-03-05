68b3e4958b802ef97c915e7bc728d8daea3b41a2
Adam Tsai <adam@visualchefs.com>
2019-03-05 18:43:36 -0500

    .

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,10 +1,11 @@
-import Nav from './Nav';
 import Link from 'next/link';
-import styled from 'styled-components';
 import Router from 'next/router';
 import NProgress from 'nprogress';
+import styled from 'styled-components';
 
 import Cart from './Cart';
+import Nav from './Nav';
+import Search from './Search';
 
 Router.onRouteChangeStart = () => {
     NProgress.start();
@@ -72,7 +73,7 @@ const Header = () => (
         </div>
 
         <div className="sub-bar">
-            <p>Search</p>
+            <Search />
         </div>
 
         <Cart />
