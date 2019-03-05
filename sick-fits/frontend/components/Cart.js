79837afe592a1493b88e86b566ab5997b8057d20
Adam Tsai <adam@visualchefs.com>
2019-03-05 13:59:45 -0500

    .

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { adopt } from 'react-adopt';
 import { Mutation, Query } from 'react-apollo';
 import gql from 'graphql-tag';
+
 
 import CartItem from './CartItem';
 import User from './User';
@@ -23,45 +25,44 @@ const TOGGLE_CART_MUTATION = gql`
     }
 `;
 
+const Composed = adopt({
+    user: ({ render }) => <User>{render}</User>,
+    toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
+    localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
+});
+
 const Cart = () => {
     return (
-        <User>
-            {({ data: { me }}) => {
+        <Composed>
+            {({ user, toggleCart, localState }) => {
+                const me = user.data.me;
+
                 if (!me) {
                     return null;
                 }
 
                 return (
-                    <Mutation mutation={TOGGLE_CART_MUTATION}>
-                        {(toggleCart) => (
-                            <Query query={LOCAL_STATE_QUERY}>
-                                {({ data }) => (
-                                    <CartStyles open={data.cartOpen}>
-                                        <header>
-                                            <CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
-                                            <Supreme>{me.name}'s Cart</Supreme>
-                                            <p>You have {me.cart.length} item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
-                                        </header>
+                    <CartStyles open={localState.data.cartOpen}>
+                        <header>
+                            <CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
+                            <Supreme>{me.name}'s Cart</Supreme>
+                            <p>You have {me.cart.length} item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
+                        </header>
 
-                                        <ul>
-                                            {me.cart.map(cartItem => (
-                                                <CartItem key={cartItem.id} cartItem={cartItem} />
-                                            ))}
-                                        </ul>
+                        <ul>
+                            {me.cart.map(cartItem => (
+                                <CartItem key={cartItem.id} cartItem={cartItem} />
+                            ))}
+                        </ul>
 
-                                        <footer>
-                                            <p>{formatMoney(calTotalPrice(me.cart))}</p>
-                                            <SickButton>Checkout</SickButton>
-                                        </footer>
-                                    </CartStyles>
-                                )}
-                            </Query>
-                        )}
-                    </Mutation>
-
+                        <footer>
+                            <p>{formatMoney(calTotalPrice(me.cart))}</p>
+                            <SickButton>Checkout</SickButton>
+                        </footer>
+                    </CartStyles>
                 );
             }}
-        </User>
+        </Composed>
     );
 };
 
