f1fcc87ad6e41c942934d2ffe1e838894b550704
Adam Tsai <adam@visualchefs.com>
2019-03-08 14:42:59 -0500

    .

--- sick-fits/frontend/components/Signup/index.js
+++ sick-fits/frontend/components/Signup/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import ErrorMessage from './ErrorMessage';
-import { CURRENT_USER_QUERY } from './User';
-import Form from './styles/Form';
+import ErrorMessage from '../ErrorMessage';
+import { CURRENT_USER_QUERY } from '../User';
+import Form from '../styles/Form';
 
 const SIGNUP_MUTATION = gql`
-    mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
+    mutation SIGNUP_MUTATION(
+        $email: String!
+        $name: String!
+        $password: String!
+    ) {
         signup(email: $email, name: $name, password: $password) {
             id
             email
@@ -21,8 +25,8 @@ class Signup extends Component {
         password: '',
         email: '',
     };
-    
-    saveToState = (e) => {
+
+    saveToState = e => {
         this.setState({ [e.target.name]: e.target.value });
     };
 
@@ -31,16 +35,21 @@ class Signup extends Component {
             <Mutation
                 mutation={SIGNUP_MUTATION}
                 variables={this.state}
-                refetchQueries={[
-                    { query: CURRENT_USER_QUERY },
-                ]}
+                refetchQueries={[{ query: CURRENT_USER_QUERY }]}
             >
                 {(signup, { error, loading }) => (
-                    <Form method="post" onSubmit={async e => {
-                        e.preventDefault();
-                        await signup();
-                        this.setState({ name: '', email: '', password: '' });
-                    }}>
+                    <Form
+                        method="post"
+                        onSubmit={async e => {
+                            e.preventDefault();
+                            await signup();
+                            this.setState({
+                                name: '',
+                                email: '',
+                                password: '',
+                            });
+                        }}
+                    >
                         <fieldset disabled={loading} aria-busy={loading}>
                             <h2>Sign Up for an Account</h2>
 
