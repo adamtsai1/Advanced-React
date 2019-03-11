bcdda1c3a32fe27a7cad40b86fe218e5c5866275
Adam Tsai <adam@visualchefs.com>
2019-03-11 15:46:44 -0400

    .

--- sick-fits/frontend/components/AddToCart/AddToCart.test.js
+++ sick-fits/frontend/components/AddToCart/AddToCart.test.js
@@ -36,6 +36,17 @@ const mocks = [
             },
         },
     },
+    {
+        request: { query: CURRENT_USER_QUERY },
+        result: {
+            data: {
+                me: {
+                    ...fakeUser(),
+                    cart: [fakeCartItem()],
+                },
+            },
+        },
+    },
 ];
 
 describe('<AddToCart />', () => {
@@ -50,5 +61,55 @@ describe('<AddToCart />', () => {
         wrapper.update();
 
         expect(toJSON(wrapper.find('button'))).toMatchSnapshot();
+    });
+
+    it('adds an item to cart when clicked', async () => {
+        let apolloClient;
+        const wrapper = mount(
+            <MockedProvider mocks={mocks}>
+                <ApolloConsumer>
+                    {client => {
+                        apolloClient = client;
+                        return <AddToCart id="abc123" />;
+                    }}
+                </ApolloConsumer>
+            </MockedProvider>
+        );
+
+        const {
+            data: { me },
+        } = await apolloClient.query({ query: CURRENT_USER_QUERY });
+
+        expect(me.cart).toHaveLength(0);
+
+        // Add an item to the cart
+        wrapper.find('button').simulate('click');
+        await wait();
+
+        // Check if the item is in the cart
+        const {
+            data: { me: meAfterAddToCart },
+        } = await apolloClient.query({ query: CURRENT_USER_QUERY });
+
+        await wait();
+
+        expect(meAfterAddToCart.cart).toHaveLength(1);
+        expect(meAfterAddToCart.cart[0].id).toBe('omg123');
+        expect(meAfterAddToCart.cart[0].quantity).toBe(3);
+    });
+
+    it('changes from add to adding when clicked', async () => {
+        const wrapper = mount(
+            <MockedProvider mocks={mocks}>
+                <AddToCart id="abc123" />;
+            </MockedProvider>
+        );
+
+        await wait();
+        wrapper.update();
+        expect(wrapper.text()).toContain('Add To Cart');
+
+        wrapper.find('button').simulate('click');
+        expect(wrapper.text()).toContain('Adding To Cart');
     });
 });
