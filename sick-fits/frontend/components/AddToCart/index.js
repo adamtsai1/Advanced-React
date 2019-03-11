278959c05ef8fcc80012daa2969faf9c927c1bad
Adam Tsai <adam@visualchefs.com>
2019-03-11 13:46:48 -0400

    .

--- sick-fits/frontend/components/AddToCart/index.js
+++ sick-fits/frontend/components/AddToCart/index.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
-import { CURRENT_USER_QUERY } from './User';
+import { CURRENT_USER_QUERY } from '../User';
 
 const ADD_TO_CART_MUTATION = gql`
-    mutation ADD_TO_CART_MUTATION ($id: ID!) {
+    mutation ADD_TO_CART_MUTATION($id: ID!) {
         addToCart(id: $id) {
             id
             quantity
@@ -21,15 +21,16 @@ class AddToCart extends React.Component {
             <Mutation
                 mutation={ADD_TO_CART_MUTATION}
                 variables={{ id }}
-                refetchQueries={[{
-                    query: CURRENT_USER_QUERY
-                }]}
+                refetchQueries={[
+                    {
+                        query: CURRENT_USER_QUERY,
+                    },
+                ]}
             >
                 {(addToCart, { loading }) => (
-                    <button
-                        disabled={loading}
-                        onClick={addToCart}
-                    >Add{loading ? 'ing': ''} To Cart</button>
+                    <button disabled={loading} onClick={addToCart}>
+                        Add{loading ? 'ing' : ''} To Cart
+                    </button>
                 )}
             </Mutation>
         );
